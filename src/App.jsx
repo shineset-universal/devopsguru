cf59e4b64859246714e5b014ab6c97d6a84897fe
Ara Zargaryan <ara.zargaryan2024@outlook.com>
2024-10-22 22:00:29 +0400

    finish

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { About } from "./components/about";
+
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <About data={landingPageData.About}/>
+      <About data={landingPageData.About1} revers />
+      <About data={landingPageData.CI_CD_Pipelines}/>
+      <About data={landingPageData.IaC_Configuration_Management} revers />
+      <About data={landingPageData.Containerization_Docker}/>
+      <About data={landingPageData.Monitoring_Logging} revers />
+      <About data={landingPageData.GitOps_Infrastructure_Automation}/>
+      <About data={landingPageData.Real_World_DevOps_Project} revers />
+      
+      <Contact data={landingPageData.Contact} />
+    </div>
+  );
+};
+
+export default App;
