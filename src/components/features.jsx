3ec00a4c661e3a5a1e96a38267d4c50661e4ffe0
AroZarg <99825574+AroZarg@users.noreply.github.com>
2024-10-27 13:59:58 +0400

    Update features.jsx

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Features = (props) => {
   return (
     <div id="features" className="text-center">
-      <div className="container">
+      <div className="container" style={{ paddingTop: '100px', paddingBottom: '100px' }}>
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Features</h2>
         </div>
